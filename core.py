a53ace0d7c36d303a71c355429446ea23fbb5235
MICHAELKELLY19 <mkelly23@hsukers.unl.edu>
2022-12-07 01:26:35 -0600

    part two and cleaned up part 1

--- core.py
+++ core.py
@@ -28,9 +28,13 @@ def validatePrompt1(val):
     return val == "1" or val == "2" or val == "3" 
 
 
+def isBlizardConditions(windSpeed, visibility, isSnowing, isSnowOnGround):
+    return windSpeed >= 35 and visibility <= (1/4) and (isSnowing or isSnowOnGround)
+
 def validateNumber(val):
     return 
 def randomConditions():
+    
     tempUpperBound = 32
     windSpeedMin = 35
     visibilityInMiles = 1/4
@@ -40,8 +44,6 @@ def randomConditions():
     
     print("You will be prompted to enter in several weather condtions and at the end will be told if they meet the criteria for a blizzard.")
     
-    temp = input("Temperature (Degrees Farenheit): ")
-    temp = float(temp)
     windSpeed = input("Wind Speed (MPH): ")
     windSpeed = float(windSpeed)
     visibility = input("Visibility (Miles): ")
@@ -53,18 +55,50 @@ def randomConditions():
     timeSpan = input("How long have these conditions been present? (Hours): ")
     timeSpan = float(timeSpan)
     
-    if temp <= 32 and windSpeed >= 35 and visibility <= (1/4) and (isSnowing or isSnowOnGround) and timeSpan >= 3:
+    if isBlizardConditions(windSpeed, visibility, isSnowing, isSnowOnGround) and timeSpan >= 3:
         print("Those conditions meet the criteria of a blizzard warning.")
     else :
-        print("Those conditions do not meet the criteria of a blizzard warning.")
+        print("Those conditions do not meet the criteria of a blizzard warning for the following reasons:\n")
+        noBlizzardReasons(windSpeed, visibility, isSnowing, isSnowOnGround, timeSpan)
+        
+def todaysWeatherConditions():
+    conditions = functions.get_conditions(349291)
+    
+    temperatue = conditions["Temperature"]["Imperial"]["Value"]
+    windSpeed = conditions["Wind"]["Speed"]["Imperial"]["Value"]
+    isSnowing = conditions["HasPrecipitation"] and conditions["PrecipitationType"] == "Snow"
+    isSnowingNY = "Yes" if isSnowing else "No"
+    visibility = conditions["Visibility"]["Imperial"]["Value"]
+    print("Wind Speed: ", windSpeed)
+    print("Is it snowing?: ", isSnowingNY)
+    print("Visibility (Miles): ", visibility)
+    
+    if isBlizardConditions(windSpeed, visibility, isSnowing, False):
+        print("These conditions meet the criteria for a Blizzard Warning. If conditions have persisted for the last three hours than a Blizzard Warning should be issued.")
+    else:
+        print("These conditions do not meet the criteria for a Blizzard Warning for the following reasons: \n")
+        noBlizzardReasons(windSpeed, visibility, isSnowing, False)
+        
+
+    
     
      
-    
+## TODO: create function that lists the reasons why the blizzard wanring does not work
+
+def noBlizzardReasons(windSpeed, visibility, isSnowing, isSnowOnGround, timeSpan=None):
+    if windSpeed < 35:
+        print("The wind speed is not fast enough.")
+    if visibility > (1/4):
+        print("The visibility is too far.")
+    if not(isSnowing or isSnowOnGround):
+        print("There is not avialable snow for the wind to blow.")
+    if (timeSpan is not None) and (timeSpan < 3):
+        print("The present conditions have not persisted long enough.")
     
     
 
 def __main__():
-    print("hello")
+    print("Hello")
     
     val = prompt1()
     
@@ -72,12 +106,11 @@ def __main__():
         val = notValidPrompt()
         
     if val == "1":
-        print("1")
         randomConditions()
     elif val =="2":
-        print("2")
-    elif val =="2":
-        print("3")
+        todaysWeatherConditions()
+    elif val =="3":
+        functions.print_HistoryData()
         
     
 
--- functions.py
+++ functions.py
@@ -1,21 +1,22 @@
 # This file will serve as a function library to be used by core.py
 # here is the curl command for the Accuweather api current conditions
 # curl -X GET "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
+api = "d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3"
 import requests
 import json
 import pandas as pd
 
 # display current conditions based on key
 def get_conditions(key):
-    conditions_url = 'https://dataservice.accuweather.com/currentconditions/v1/'f'?apikey={api}'.format(key)
+    conditions_url = "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
     response = requests.get(conditions_url)
     json_version = response.json()
-    print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
-    return json_version
+    #print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
+    return json_version[0]
     
 def print_HistoryData():
     pd.set_option('display.max_rows', None)
-    df = pd.read_csv (r'OmahaWeatherHistory copy 2.csv')
+    df = pd.read_csv (r'OmahaWeatherHistory copy.csv')
     print("Eppley Airfield Historic Weather Data\n")
     print("Cordinates:(41.301475,-95.894521) \n")
     df.pop('dt') 
