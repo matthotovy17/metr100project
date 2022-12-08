ba27c140c04cbb86648a634b0e8b6cf45df84a2e
MICHAELKELLY19 <mkelly23@hsukers.unl.edu>
2022-12-08 08:21:56 -0600

    few minor changes

--- core.py
+++ core.py
@@ -4,6 +4,8 @@
 import requests
 import json
 import functions
+import pandas as pd
+from tabulate import tabulate
 
 # get the current conditions for Omaha: 349291 is the location key for Omaha
 #response = functions.get_conditions(349291)
@@ -27,7 +29,11 @@ def notValidPrompt():
 def validatePrompt1(val):
     return val == "1" or val == "2" or val == "3" 
 
-
+def blizardConditionsBlurb():
+    print("""\n\nThere are 3 major criteria for a blizard warning:
+          - Large amounts of snow or blowing snow.
+          - Winds greater than 35 mph.
+          - Visibility below a quarter of a mile for at least 3 hours.""")
 def isBlizardConditions(windSpeed, visibility, isSnowing, isSnowOnGround):
     return windSpeed >= 35 and visibility <= (1/4) and (isSnowing or isSnowOnGround)
 
@@ -56,10 +62,25 @@ def randomConditions():
     timeSpan = float(timeSpan)
     
     if isBlizardConditions(windSpeed, visibility, isSnowing, isSnowOnGround) and timeSpan >= 3:
-        print("Those conditions meet the criteria of a blizzard warning.")
+        print("Those conditions meet the criteria of a blizzard warning.\n")
     else :
         print("Those conditions do not meet the criteria of a blizzard warning for the following reasons:\n")
         noBlizzardReasons(windSpeed, visibility, isSnowing, isSnowOnGround, timeSpan)
+        
+    blizardConditionsBlurb()
+    
+    print("""\n\n Do you want to try again? (Y) 
+          Go back to the main menu? (menu) 
+          or Quit the application? (quit)""")
+    
+    val = input()
+    
+    if val == "Y":
+        randomConditions()
+    elif val == "menu":
+        __main__()
+    elif val == "quit":
+        exit()
         
 def todaysWeatherConditions():
     conditions = functions.get_conditions(349291)
@@ -78,6 +99,16 @@ def todaysWeatherConditions():
     else:
         print("These conditions do not meet the criteria for a Blizzard Warning for the following reasons: \n")
         noBlizzardReasons(windSpeed, visibility, isSnowing, False)
+    
+    blizardConditionsBlurb()
+    
+    print("""Would you like to 'quit' the appilcation or go to the main 'menu'? """)
+    val = input()
+    
+    if val == 'quit':
+        exit()
+    elif val == "menu":
+        __main__()
         
 
     
@@ -95,6 +126,136 @@ def noBlizzardReasons(windSpeed, visibility, isSnowing, isSnowOnGround, timeSpan
     if (timeSpan is not None) and (timeSpan < 3):
         print("The present conditions have not persisted long enough.")
     
+
+def historicalWeatherData(fromCounty=False):
+    mainDisplay = pd.read_csv(r'mainDisplay.csv')
+    historicalData = pd.read_csv(r'newWeatherHistory.csv')
+    alerts = pd.read_csv(r'cleanBZAlertNew.csv')
+    codes = pd.read_csv(r'ugcCode.csv')
+    times = pd.read_csv(r'uniqueStartAndEndTimes.csv')
+    
+    if fromCounty:
+        countyRoute(codes, historicalData, mainDisplay)
+    
+    print("""Welcome time traveler! Below is a list of all the blizzard warnings issued by OAX in the last 20 years.
+          Additionally, we have done some aggregates of the weather while the warning was in affect. 
+          If you would like to see the hour by hour weather during one of the alerts just type in the number.
+          If you would like to only see the warnings for a specific county type in 'c'.
+          If you would like to return to the main menu, type in 'menu'.
+          If you would like to quit the application, type in 'quit'.""")
+    
+    mdt = mainDisplay
+    
+    ["Total", "Column", round(mdt['Temperature Min'].mean(), 2), round(mdt['Temperature Max'].mean(), 2), round(mdt['Temperature Avg'].mean(), 2),  
+                               round(mdt['Wind Spd Min'].mean(), 2), round(mdt['Wind Spd Max'].mean(), 2), round(mdt['Wind Spd Avg'].mean(), 2),
+                               round(mdt['Visibility Min'].mean(), 2), round(mdt['Visibility Max'].mean(), 2), round(mdt['Visibility Avg'].mean(), 2),
+                               sum(mdt['Had Snowfall']), []]
+    
+    print("""Averages of values from the table:
+          Average Temperature Min: {}
+          Average Temperature Max: {}
+          Average Temperature Avg: {}
+          Average Wind Spd Min: {}
+          Average Wind Spd Max: {}
+          Average Wind Spd Avg: {}
+          Average Visibility Min: {}
+          Average Visibility Max: {}
+          Average Visibility Avg: {}
+          Number of Alerts with Snowfall: {}""".format(round(mdt['Temperature Min'].mean(), 2), round(mdt['Temperature Max'].mean(), 2), round(mdt['Temperature Avg'].mean(), 2),  
+                               round(mdt['Wind Spd Min'].mean(), 2), round(mdt['Wind Spd Max'].mean(), 2), round(mdt['Wind Spd Avg'].mean(), 2),
+                               round(mdt['Visibility Min'].mean(), 2), round(mdt['Visibility Max'].mean(), 2), round(mdt['Visibility Avg'].mean(), 2),
+                               sum(mdt['Had Snowfall'])))
+    print(tabulate(mainDisplay.loc[:, mainDisplay.columns != "counties"], headers='keys', tablefmt='psql'))
+    
+    val = input()
+    
+    if val == "c":
+        countyRoute(codes, historicalData, mainDisplay)
+    elif val == "menu":
+        __main__()
+    elif val == "quit":
+        exit()
+    elif val.isdigit():
+        num = int(val)
+        hourByHour(mainDisplay.iloc[num], historicalData)
+        
+def hourByHour(mdRow, weatherHistory, fromCounty=False): 
+    
+    
+    startDate = mdRow["Issue DT"]
+    endDate = mdRow["Expire DT"]
+    print("Now showing hour by hour from {} to {}".format(startDate, endDate))
+    searchStart = functions.formatSearchTime(startDate, False)
+    searchEnd = functions.formatSearchTime(endDate, True)
+    
+    subWeatherHistory = weatherHistory[(weatherHistory['dt_iso'] >= searchStart) & (weatherHistory['dt_iso'] <= searchEnd)]
+    displayHBH = functions.getDisplayHourByHour(subWeatherHistory)
+    
+    print(tabulate(displayHBH, headers='keys', tablefmt='psql'))
+    
+    print("""If you would like to go back to the main menu type in 'menu'.
+          If you would like to go back to the aggregate table type in 'back'.
+          If you would like to quit the application type 'quit'\n\n""")
+    if fromCounty:
+        print("You can also go back and slect another county by typing in 'c'.")
+    val = input()
+    
+    if val == 'menu':
+        __main__()
+    elif val == "back":
+        historicalWeatherData()
+    elif val == "quit":
+        exit()
+    elif val == "c":
+        historicalWeatherData(True)
+    
+
+def countyRoute(codes, historicalData, mainDisplay):
+    print("""Below are a list of counties that blizzard warnings were sent out to.
+          You can select a county by typing in the corresponding number.
+          This will filter down the alerts to the specific counties.
+          Or you can 'quit', go 'back' to the aggregate board, or go back to the 'menu'.""")
+    
+    print(tabulate(codes[['County']], headers='keys', tablefmt='psql'))
+    val = input()
+    
+    if val == 'quit':
+        exit()
+    elif val == "back":
+        historicalWeatherData()
+    elif val == "menu":
+        __main__()
+    elif val.isdigit():
+        val = int(val)
+        countyRow = codes.iloc[val]
+        filterByCounty(countyRow, historicalData, mainDisplay)
+    
+    
+    
+def filterByCounty(countyRow, historicalData, mainDisplay):
+    print("You have chose alerts for {} County".format(countyRow['County']))
+    
+    print("""From here you can select another county by typing in 'c'.
+          'quit' the application. Go to the main 'menu'.
+          Or select a blizzard warning and see the hour by hour by typing in the number associated with it""")
+    
+    mdCounty = mainDisplay[[countyRow['Z'] in i for i in mainDisplay['counties']]]
+    
+    print(tabulate(mdCounty.loc[:, mdCounty.columns != "counties"], headers='keys', tablefmt='psql'))
+    
+    val = input();
+    
+    if val == "c":
+        historicalWeatherData(True)
+    elif val == "quit":
+        exit()
+    elif val == "menu":
+        __main__()
+    elif val.isdigit():
+        val = int(val)
+        mdRow = mainDisplay.iloc[val]
+        hourByHour(mdRow, historicalData, True)
+    
     
 
 def __main__():
@@ -110,11 +271,8 @@ def __main__():
     elif val =="2":
         todaysWeatherConditions()
     elif val =="3":
-        functions.print_HistoryData()
+        historicalWeatherData()
         
-    
-
-    
-
-
 __main__()
+
+print("Thanks for joining us! Have a great day!")
--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ api = "d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3"
 import requests
 import json
 import pandas as pd
-
+from tabulate import tabulate
 # display current conditions based on key
 def get_conditions(key):
     conditions_url = "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
@@ -32,3 +32,117 @@ def print_HistoryData():
     df.pop('weather_id')
     df.pop('clouds_all')
     print(df)
+
+def createNewAlertCsv():
+    pd.set_option('display.max_rows', None)
+    df = pd.read_csv(r'cleanBZAlert.csv')
+    test = df[df['ugc'].str.contains('NEZ')]
+    test.to_csv(r'./cleanBZAlert2.csv', index=False)
+    print(test['ugc'])
+    #print(filteredDF)
+
+def createNewWeatherDataCsv():
+    pd.set_option('display.max_rows', None)
+    
+    df = pd.read_csv(r'OmahaWeatherHistory copy.csv')
+    
+    dates = pd.read_csv(r'uniqueStartAndEndTimes.csv')
+    
+    startDates = dates['utc_issue'].str[0:10]
+    endDates = dates['utc_expire'].str[0:10]
+    
+    totalDates = startDates.append(endDates)
+    uniqueDates = totalDates.unique()
+    print(len(uniqueDates))
+    
+    filteredDF = df[df['dt_iso'].str[0:10].isin(uniqueDates)]
+    # filteredDF.to_csv(r'newWeatherHistory.csv', index=False)
+    print(tabulate(filteredDF, headers='keys', tablefmt='psql'))
+    
+def createMainDisplayCSV(): 
+    print("main display csv")
+    
+    weatherHistory = pd.read_csv(r'newWeatherHistory.csv')
+    dates = pd.read_csv(r'uniqueStartAndEndTimes.csv')
+    alerts = pd.read_csv(r'cleanBZAlertNew.csv')
+    codes = pd.read_csv(r'ugcCode.csv')
+    
+    mainDisplay = pd.DataFrame(columns=["Issue DT", "Expire DT", "Temperature Min", "Temperature Max", 
+                                        "Temperature Avg", "Wind Spd Min", "Wind Spd Max","Wind Spd Avg", 
+                                        "Visibility Min", "Visibility Max", "Visibility Avg", "Had Snowfall", "counties"])
+    print(weatherHistory.columns)
+    
+    for index, row in dates.iterrows():
+        startTime = row["utc_issue"]
+        endTime = row["utc_expire"]
+        
+        startSearch = formatSearchTime(startTime, False)
+        endSearch = formatSearchTime(endTime, True)
+        
+        subWeatherHistory = weatherHistory[(weatherHistory['dt_iso'] >= startSearch) & (weatherHistory['dt_iso'] <=endSearch)]
+        
+        minTemp = subWeatherHistory['temp'].min()
+        maxTemp = subWeatherHistory['temp'].max()
+        avgTemp = round(subWeatherHistory['temp'].mean(), 2)
+        
+        minWind = subWeatherHistory['wind_speed'].min()
+        maxWind = subWeatherHistory['wind_speed'].max()
+        avgWind = round(subWeatherHistory['wind_speed'].mean(), 2)
+        
+        minVis = subWeatherHistory['visibility'].min()
+        maxvis = subWeatherHistory['visibility'].max()
+        avgVis = round(subWeatherHistory['visibility'].mean(), 2)
+        
+        hadSnowFall = (True in set(subWeatherHistory['weather_main'].str.contains("Snow")) )| (True in set(subWeatherHistory['weather_description'].str.contains("Snow")))
+        
+        counties = getCounties(startTime, endTime, alerts, codes)
+        mainDisplay.loc[len(mainDisplay.index)] = [startTime, endTime, minTemp, maxTemp, avgTemp, minWind, maxWind, avgWind, minVis, maxvis, avgVis, hadSnowFall, counties]
+    
+    # mainDisplay.loc[len(mainDisplay.index)] = ["Total", "Column", round(mainDisplay['Temperature Min'].mean(), 2), round(mainDisplay['Temperature Max'].mean(), 2), round(mainDisplay['Temperature Avg'].mean(), 2),  ]
+    mainDisplay.to_csv(r'mainDisplay.csv', index=False)
+        
+    print(tabulate(mainDisplay, headers='keys', tablefmt='psql'))
+
+def getCounties(startTime, endTime, alerts, codes):
+
+    relevantAlerts = alerts[(alerts['utc_issue'] == startTime) & (alerts["utc_expire"] == endTime)]
+    
+    joined = pd.merge(relevantAlerts, codes, how="left", left_on="ugc", right_on="Z")
+    
+    return list(joined['Z'])
+    
+    
+        
+def getDisplayHourByHour(weatherHistory):
+    return weatherHistory[["dt_iso", "temp", "visibility", "wind_speed", "weather_main", "weather_description"]]
+    
+    
+        
+        
+        
+
+
+def formatSearchTime (dt, isEndTime):
+    if isEndTime:
+        date = dt[0:10]
+        hr = int(dt[11:13])
+        minute = int(dt[14:16])
+        
+        if minute > 0:
+            hr +=1
+        
+        hr = str(hr)
+        
+        if len(hr) == 1:
+            hr = "0{}".format(hr)
+        newDt = "{} {}:00:00 +0000 UTC".format(date, hr)
+        return newDt
+    else:
+        date = dt[0:10]
+        hr = dt[11:13]
+        newDt = "{} {}:00:00 +0000 UTC".format(date, hr)
+        return newDt
+        
+        
+        
+    
